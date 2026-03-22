9c794fcbfaba31d0792a1fd4cb5dcf9131bbafd7
d3myasha <beebo.c4t@gmail.com>
2026-03-22 21:00:08 +1000

    bug fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    allowedHosts: true
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
